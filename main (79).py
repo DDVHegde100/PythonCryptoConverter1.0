36d025434a08be299c190668f5c7d171bcd0eff9
Dhruv Hegde <69629876+DDVHegde100@users.noreply.github.com>
2021-08-08 10:14:30 -0400

    Add files via upload

---
+++ main (79).py
@@ -0,0 +1,6 @@
+print("Welcome to Dhruv's Cruptocurrency Converter!")
+val=str(input('Enter the output cryptocurrency code:'))
+if(val=='BTC'):
+    USD=float(input('Enter the amount in USD:'))
+    BTC=USD*0.000022
+    print('%0.3f USD is equal to %0.3f bitcoin' %(USD, BTC))
